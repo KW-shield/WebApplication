85b3e5f08953ec1fd2b215fb32104a6fb0d39432
ChanYong Park <qkrcksdyd99@gmail.com>
2023-02-14 06:02:39 +0900

    add alert function

---
+++ static/script.js
@@ -0,0 +1,19 @@
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+
+const alert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible col-6 mx-auto" role="alert">`,
+    `   <div>${message}</div>`,
+    "</div>",
+  ].join("");
+
+  alertPlaceholder.append(wrapper);
+};
+
+const alertTrigger = document.getElementById("liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    alert("  Waiting for results...", "primary");
+  });
+}
