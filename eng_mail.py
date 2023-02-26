647857aea4da4ed70b4e20ee85a8eccd1541ffe6
ChanYong Park <qkrcksdyd99@gmail.com>
2023-02-27 04:39:36 +0900

    modify eng.py

--- eng_mail.py
+++ eng_mail.py
@@ -1,23 +1,21 @@
 import torch
-import tensorflow as tf
 import numpy as np
-import pandas as pd
-import re
-import konlpy
+import tensorflow as tf
 import nltk
 import urlextract
+import re
 import url
 import ssl
 import socket
 
 from html import unescape
+from nltk.corpus import stopwords
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from konlpy.tag import Okt, Kkma
-from hanspell import spell_checker
+from transformers import BertForSequenceClassification
+from transformers import BertTokenizer
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler
-from transformers import BertTokenizer, BertForSequenceClassification
-nltk.download('punkt')
-url_extractor = urlextract.URLExtract()  # url 추출
+from tensorflow.python.ops.numpy_ops import np_config
+np_config.enable_numpy_behavior()
 
 
 def html_to_plain_text(html):  # HTML을 일반 텍스트로 변환하는 함수
@@ -53,7 +51,64 @@ def email_to_text(email):        # 포멧에 상관없이 이메일을 입력받
         return email
 
 
+stemmer = nltk.PorterStemmer()  # 어간 추출
+url_extractor = urlextract.URLExtract()  # url 추출
+nltk.download('stopwords')  # 불용어
+stop_words = stopwords.words('english')
+
+re_space = re.compile(r'[\W+_]')  # 스페이스바를 나타내는 정규표현식
+re_delete_numberspot = re.compile(r'[,]')  # ','을 나타내는 정규표현식
+
+# re_arrange_email과 re_email을 사용하여 이메일을 나타내는 정규표현식
+re_arrange_email = re.compile(r'[^\w.@-]')
+re_email = re.compile('[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+
+
+def preprocess(mail):
+    X_transformed = []
+    all_url = []  # 메일안의 url을 저장하는 list
+
+    text = email_to_text(mail) or ""  # HTML 관련 태그 제거
+
+    text = re.sub(re_delete_numberspot, '', text)  # 숫자 사이의 ',' 제거
+
+    # url 추출
+    urls = list(set(url_extractor.find_urls(text)))
+    urls.sort(key=lambda url: len(url), reverse=True)
+    all_url.append(urls)
+    # 해당 url을 모두 ""로 변경
+    for url in urls:
+        text = text.replace(url, "")
+
+    text = text.lower()  # 모든 문자를 소문자화
+
+    # 이메일 추출
+    text = re.sub(re_arrange_email, ' ', text)
+    # 해당 email을 모두 ""로 변경
+    text = re.sub(re_email, '', text)
+
+    # 숫자와 문자를 제외한 것을 띄어쓰기로 변경
+    text = re.sub(re_space, ' ', text)
+    text = ' '.join(text.split())
+
+    # 불용어 제거
+    text = text.split()
+    text = [word for word in text if not word in stop_words]
+
+    # 길이가 2보다 작은 단어 제거
+    text = [word for word in text if len(word) > 2]
+
+    # 어간 추출
+    text = [stemmer.stem(word) for word in text]
+    text = ' '.join(text)
+
+    X_transformed.append(text)
+
+    return np.array(X_transformed), all_url
+
 ## HTTPS 연결 요청 함수 ##
+
+
 def https_connect(url):
     ctx = ssl.create_default_context()
     s = ctx.wrap_socket(socket.socket(), server_hostname=url)
@@ -87,85 +142,9 @@ def attach_http(urls):
     return url
 
 
-# url을 나타내는 정규표현식
-re_url = re.compile(r'\shttps?://www\.[a-z0-9-]+\.[a-z0-9]+\.[a-z0-9]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\swww\.[a-z0-9-]+\.[a-z0-9]+\.[a-z0-9]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\shttps?://[a-z0-9-]+\.[a-z0-9]+\.[a-z0-9]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\s[a-z0-9-]+\.[a-z0-9]+\.[a-z0-9]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\shttps?://www\.[a-z0-9-]+\.[a-z0-9]+\.[a-z0-9]+\.[a-z]{2,4}[/]?|'
-                    '\swww\.[a-z0-9-]+\.[a-z0-9]+\.[a-z0-9]+\.[a-z]{2,4}[/]?|'
-                    '\shttps?://[a-z0-9-]+\.[a-z0-9]+\.[a-z0-9]+\.[a-z]{2,4}[/]?|'
-                    '\s[a-z0-9-]+\.[a-z0-9]+\.[a-z0-9]+\.[a-z]{2,4}[/]?|'
-                    '\shttps?://www\.[a-z0-9-]+\.[a-z0-9]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\swww\.[a-z0-9-]+\.[a-z0-9]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\shttps?://[a-z0-9-]+\.[a-z0-9]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\s[a-z0-9-]+\.[a-z0-9]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\shttps?://www\.[a-z0-9-]+\.[a-z0-9]+\.[a-z]{2,4}[/]?|'
-                    '\swww\.[a-z0-9-]+\.[a-z0-9]+\.[a-z]{2,4}[/]?|'
-                    '\shttps?://[a-z0-9-]+\.[a-z0-9]+\.[a-z]{2,4}[/]?|'
-                    '\s[a-z0-9-]+\.[a-z0-9]+\.[a-z]{2,4}[/]?|'
-                    '\shttps?://www\.[a-z0-9-]+\..[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\swww\.[a-z0-9-]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\shttps?://[a-z0-9-]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\s[a-z0-9-]+\.[a-z]{2,4}\/[a-z0-9.!"#%&()\-+\,/:;<>=?@{}`|~\[\]]+|'
-                    '\shttps?://www\.[a-z0-9-]+\.[a-z]{2,4}[/]?|'
-                    '\swww\.[a-z0-9-]+\.[a-z]{2,4}[/]?|'
-                    '\shttps?://[a-z0-9-]+\..[a-z]{2,4}[/]?|'
-                    '\s[a-z0-9-]+\.[a-z]{2,4}[/]?', flags=re.I)
-
-
-def cleaning(mail):
-    X_transformed = []
-    all_url = []
-    clean_text = email_to_text(mail) or ""  # HTML 관련 태그 제거
-
-    # url 추출
-    re_kor = re.compile(r'[가-힣]+')  # 한글을 나타내는 정규표현식
-    non_kor = re.sub(re_kor, ' ', clean_text)
-    urls1 = re.findall(re_url, non_kor)
-    urls2 = list(set(url_extractor.find_urls(non_kor)))
-    urls = urls1 + urls2
-    urls = ' '.join(s for s in urls)
-    urls = list(set(url_extractor.find_urls((urls))))
-    all_url.append(urls)
-    for url in urls:
-        clean_text = clean_text.replace(url, "")
-
-    # 기본적인 전처리
-    clean_text = re.sub(r'[^ ㄱ-ㅣ가-힣]', '', clean_text)
-    clean_text = clean_text.lower()
-    clean_text = re.sub(r'\s+', ' ', clean_text)
-
-    # 띄어쓰기, 맞춤법 검사
-    spelled_sent = spell_checker.check(clean_text)
-    clean_text = spelled_sent.checked
-
-    # 형태소 분리
-    okt = konlpy.tag.Okt()
-    rmv_morpheme = []
-    for word in okt.pos(clean_text, stem=True):
-        if word[1] in ['Noun', 'Verb', 'Adjective']:
-            rmv_morpheme.append(word[0])
-    clean_text = ' '.join(rmv_morpheme)
-
-    # 불용어 제거
-    df = pd.read_csv('stopword.txt', header=None)
-    df[0] = df[0].apply(lambda x: x.strip())
-    stopwords = df[0].to_numpy()
-    rmv_stopword = []
-    for word in nltk.tokenize.word_tokenize(clean_text):
-        if word not in stopwords:
-            rmv_stopword.append(word)
-    clean_text = ' '.join(rmv_stopword)
-
-    X_transformed.append(clean_text)
-
-    return np.array(X_transformed), all_url
-
-
 def predict(email):
     MAX_LEN = 512
-    X_NLP, URL = cleaning(email)
+    X_NLP, URL = preprocess(email)
     url_in_mail = URL
     # 메일 내에 URL이 존재하는 경우
     if URL:
@@ -190,8 +169,8 @@ def predict(email):
         for i in URL:
             url_pred.append(url.url_check(i))
 
-    X_NLP = ''.join(X_NLP)
     # 메일에 [CLS] 와 [SEP] 추가
+    X_NLP = ''.join(X_NLP)
     sentences = "[CLS] " + str(X_NLP) + " [SEP]"
 
     tokenizer = BertTokenizer.from_pretrained(
@@ -228,7 +207,6 @@ def predict(email):
     test_dataloader = DataLoader(test_data)
 
     input, masks, labels = next(iter(test_dataloader))
-
 
     # model에 입력할 값들, URL 예측값, 메일에 포함된 URL 반환
     return input, masks, labels, url_pred, url_in_mail
